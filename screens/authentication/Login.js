322ea314d4b12fb8c2ebb9c17ba1bc68f36a0cfd
njokuScript <kalunjoku123@gmail.com>
2020-03-06 15:26:06 +0100

    working on login

--- screens/authentication/Login.js
+++ screens/authentication/Login.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export class Login extends Component {
   render() {
     return (
-      <View>
-        <Text> Login </Text>
+      <View style={styles.container}>
+        <View style={styles.TextContainer}>
+          <Text style={styles.Text}> Oya Send </Text>
+        </View>
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  Text: {
+    fontSize: 28,
+    fontStyle: "bold",
+    textAlign: "center",
+    marginTop: "0",
+    width: "200"
+  }
+});
 export default Login;
